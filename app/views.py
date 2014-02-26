42b367ac1abf4c278a098f2a7585dca9c28452ef
Amanpreet Singh <aps.sids@gmail.com>
2014-02-26 01:15:42 +0000

    Added Chart.JS basic graphs

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect
 from app.database import db_session
 from app.models import Sensor, Data
 from app import app
@@ -6,6 +6,7 @@ from flask_sockets import Sockets
 from sqlalchemy import desc
 from json import dumps
 from time import sleep
+import random
 
 sockets = Sockets(app)
 
@@ -16,8 +17,21 @@ def shutdown_session(exception=None):
 
 
 @app.route('/')
-def index():
-    return app.static_folder
+def redirectToFirstSensor():
+    return redirect('/1')
+
+
+@app.route('/<id>')
+def index(id):
+    if Sensor.query.filter_by(id=id).count():
+                queryr = Data.query.filter_by(sensor_id=id).order_by(desc(Data.time)).limit(100).all()
+                data = map(dict, queryr)
+                values = [e["value"] for e in data]
+                #times = [e["time"][-8:] for e in data]
+                values2 = values[:]
+                random.shuffle(values2)
+    return render_template('graph.html', values=values, values2=values2)
+    #return app.static_folder
 
 
 @app.route('/sensor')
@@ -29,6 +43,14 @@ def home():
 def sensor(id):
     return render_template('data.html',
                            data=Data.query.filter(Data.sensor_id == id))
+
+@app.route('/sensor/data/<id>')
+def sensordata(id):
+    if Sensor.query.filter_by(id=id).count():
+                queryr = Data.query.filter_by(sensor_id=id).order_by(desc(Data.time)).limit(100).all()
+                return dumps(map(dict, queryr))
+    
+
 
 
 @sockets.route('/websocket')
